55e1d5b24a3a95d8ce311d14b19d19ddaef4f752
Ngô Thượng Hiếu <87613450+hieunt1410@users.noreply.github.com>
2024-03-15 09:49:38 +0700

    Update hieu.py

--- Day 3 - Product of Array Except Self (Medium)/hieu.py
+++ Day 3 - Product of Array Except Self (Medium)/hieu.py
@@ -1,3 +1,5 @@
+#**Method 1**
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         if nums.count(0) == len(nums):
@@ -23,3 +25,21 @@ class Solution:
                 else:
                     ret.append(hehe)
         return ret
+
+#**Method 2** 
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        cnt = nums.count(0)
+        n = len(nums)
+        prefix = [1] * (n + 1)
+        suffix = [1] * (n + 1)
+        for i in range(1, n+1):
+            prefix[i] = prefix[i-1] * nums[i-1]
+            suffix[i] = suffix[i-1] * nums[n-i]
+        ret = [0] * n
+        for i in range(1, n+1):
+            print(i,prefix[i-1], suffix[n-i])
+            ret[i-1] = prefix[i-1] * suffix[n-i]
+        print(prefix)
+        print(suffix)
+        return ret 
