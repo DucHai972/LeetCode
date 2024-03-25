f3b223c3ae5291eba17c2afe5fb0fb0230e01572
HHHMGK <65905035+HHHMGK@users.noreply.github.com>
2024-03-25 09:01:53 +0000

    update note

--- Day 12 - Find the Duplicate Number (Medium)/huy.cpp
+++ Day 12 - Find the Duplicate Number (Medium)/huy.cpp
@@ -1,9 +1,10 @@
 // Floyd's turtoise and hare in detecting cycle
 // idea from https://youtu.be/pKO9UjSeLew
-// Think the number array as graph, a[i] = x means edge(i,x)
 // Verticies are 0-indexed
-// Found a circle = There's a duplicate number 
-// because of the constrain and declaration of the problem
+// Think the number array as graph, a[i] = x means edge(i,x)
+// Found a circle = There's a duplicate number i.e there's more than 1 edge 
+// pointing to an vertex (more than one i that a[i] = x)
+// The duplicated number will be the begining of the cycle
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
