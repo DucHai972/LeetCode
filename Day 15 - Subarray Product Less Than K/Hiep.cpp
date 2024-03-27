17e992f45acfddc3815d6f6c920aa666086c49b5
Trịnh Đức Hiệp <113037834+DucHiepisme@users.noreply.github.com>
2024-03-27 23:40:05 +0700

    Create Hiep.cpp

---
+++ Day 15 - Subarray Product Less Than K/Hiep.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int left = 0;
+        int right = 0;
+        int product = 1;
+        int count = 0;
+        int n = nums.size();
+        if(k <= 1) return 0;
+        while(right < n){
+            product *= nums[right];
+            while(product >= k) product /= nums[left++];
+            count += (right - left) + 1;
+            right++;
+        } 
+        return count;
+    }
+};
