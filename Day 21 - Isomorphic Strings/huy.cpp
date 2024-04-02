ad3de12f59e55a0a05dcc303b4a3201043d87f0d
HHHMGK <65905035+HHHMGK@users.noreply.github.com>
2024-04-02 04:24:52 +0000

    hhh

---
+++ Day 21 - Isomorphic Strings/huy.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> m;
+        bool md[256] = {};
+        if(s.size() != t.size()) return 0;
+        for(int i=0;i<s.size();i++)
+            if(m.find(s[i]) == m.end()){
+                if(md[t[i]]) return 0;
+                m[s[i]] = t[i];
+                md[t[i]] = 1;
+            } else if(m[s[i]] != t[i]) return 0;
+                    
+        return 1;
+    }
+};
